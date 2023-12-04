7641815b91f846441a3d26745371837da50ebc78
JosephJoshua <jj.anggita@gmail.com>
2023-12-04 21:26:58 +0700

    feat: add role

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,18 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::before(function (User $user) {
+            if ($user->role === 'admin') {
+                return true;
+            }
+        });
+
+        Gate::define('admin', function (User $user) {
+            return $user->role === 'admin';
+        });
+
+        Gate::define('teacher', function (User $user) {
+            return $user->role === 'teacher';
+        });
     }
 }
